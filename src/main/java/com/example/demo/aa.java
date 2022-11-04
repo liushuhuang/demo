c41946099890c57477aa7bec2ab4dc1bf31a457e
liushuhuang <2100787301@qq.com>
2022-11-04 23:32:05 +0800

    1

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -3,9 +3,9 @@ package com.example.demo;
 public class aa {
 	public int aa;
 	void soa(){
-		System.out.println("b分支修改的11111代码");
-		System.out.println("b分支的新增2222的代码");
+		System.out.println("b分支的新增的代码");
 		System.out.println("b分支的第二次新增的代码");
+		System.out.println("这是aaaa分支第二次的代码");
 		System.out.println("1111");
 	}
 }
