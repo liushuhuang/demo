c66e8a2f4401920eb5800e1942a501dac7fb4f81
liushuhuang <2100787301@qq.com>
2022-11-04 23:32:05 +0800

    forth

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -3,9 +3,8 @@ package com.example.demo;
 public class aa {
 	public int aa;
 	void soa(){
-		System.out.println("这是aa的原代码");
-		System.out.println("b分支修改的代码");
-		System.out.println("b分支的新增的代码");
+		System.out.println("b分支修改的11111代码");
+		System.out.println("b分支的新增2222的代码");
 		System.out.println("b分支的第二次新增的代码");
 		System.out.println("1111");
 	}
