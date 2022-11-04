fe4bf0fa4078624f539eabfe0ac368136eb09302
liushuhuang <2100787301@qq.com>
2022-11-05 01:12:13 +0800

    4

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -4,8 +4,7 @@ public class aa {
 	public int aa;
 	void soa(){
 		System.out.println();
-		System.out.println();
+		System.out.println("a");
 		System.out.println("aa1111aa");
-		System.out.println("这是newbranch的代码");
 	}
 }
