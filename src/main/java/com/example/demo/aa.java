e5b15f4a293146024b341b51d31b54edd2e3c3e7
liushuhuang <2100787301@qq.com>
2022-11-05 20:41:53 +0800

    8

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -6,6 +6,6 @@ public class aa {
 		System.out.println();
 		System.out.println();
 		System.out.println("aa1111aa");
-		System.out.println("这是master的原代码");
+		System.out.println("这是修改过后的new代码");
 	}
 }
