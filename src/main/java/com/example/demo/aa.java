7bbdf51d21ed7f3ec890a1c9b9307a9116814569
liushuhuang <2100787301@qq.com>
2022-11-05 21:38:06 +0800

    8

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -4,6 +4,7 @@ public class aa {
 	public int aa;
 	void soa(){
 		System.out.println("这是aa的原代码");
-		System.out.println("aa的开发代码");
+		System.out.println("b分支修改的代码");
+		System.out.println("b分支的新增的代码");
 	}
 }
--- src/main/java/com/example/demo/bb.java
+++ src/main/java/com/example/demo/bb.java
@@ -5,5 +5,7 @@ public class bb {
 	void sob(){
 		System.out.println("这是aa的原代码");
 		System.out.println("aa的开发代码");
+		System.out.println("b分支修改的代码");
+		System.out.println("b分支的新增的代码");
 	}
 }
