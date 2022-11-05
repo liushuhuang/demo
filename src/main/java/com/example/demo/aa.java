084d6c3012f6d3f74dc266fcc1b4885537d1f5f1
liushuhuang <2100787301@qq.com>
2022-11-05 21:34:20 +0800

    8

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -3,8 +3,6 @@ package com.example.demo;
 public class aa {
 	public int aa;
 	void soa(){
-		System.out.println();
-		System.out.println("aa1111aa");
-		System.out.println("这是master的原代码");
+		System.out.println("这是aa的原代码");
 	}
 }
--- src/main/java/com/example/demo/bb.java
+++ src/main/java/com/example/demo/bb.java
@@ -3,13 +3,6 @@ package com.example.demo;
 public class bb {
 	public int bb;
 	void sob(){
-		System.out.println("bbbbb");
-		System.out.println("ddddd");
-		System.out.println("ccccc");
-		System.out.println("这是master的原代码");
-		System.out.println();
-
-
-
+		System.out.println("这是aa的原代码");
 	}
 }
