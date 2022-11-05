3f7dbc1b5c78d91cd90124d867650f14a259a58f
liushuhuang <2100787301@qq.com>
2022-11-05 20:40:12 +0800

    7

--- src/main/java/com/example/demo/bb.java
+++ src/main/java/com/example/demo/bb.java
@@ -6,7 +6,7 @@ public class bb {
 		System.out.println("bbbbb");
 		System.out.println("ddddd");
 		System.out.println("ccccc");
-		System.out.println("这是master的原代码");
+		System.out.println("这是修改过后的new代码");
 		System.out.println();
 		System.out.println();
 	}
